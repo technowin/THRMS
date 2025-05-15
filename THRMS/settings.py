ab17f944900338f03ed738780c85fefaedfa2e7b
technowin <technowinitinfra@gmail.com>
2025-05-15 16:54:16 +0530

    fgfgf

--- THRMS/settings.py
+++ THRMS/settings.py
@@ -40,11 +40,19 @@ DATABASES = {
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # or your desired path
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+   
+# ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'D:/Python Projects/Documents/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'C:/inetpub/wwwroot/Documents/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'C:/inetpub/wwwroot/Documents/')
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
@@ -133,13 +141,6 @@ AUTO_LOGOUT = {
     'REDIRECT_TO_LOGIN_IMMEDIATELY': True,
     'MESSAGE': 'The session has expired. Please login again to continue.',
 }
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-   
-]
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 LOGGING = {
     'version': 1,
