d21357a9000acacd08d28288eaac3ba47a1d957d
technowin <technowinitinfra@gmail.com>
2025-05-22 14:08:28 +0530

    ghfhff

--- THRMS/urls.py
+++ THRMS/urls.py
@@ -32,8 +32,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # APP URLS
 
-    # path('upload/', upload_document, name='upload_document'),
-    # path('document/<int:pk>/', document_detail, name='document_detail'),
+    path('upload/', upload_document, name='upload_document'),
+    path('document_detail1/<int:pk>/', document_detail1, name='document_detail1'),
+    
     path('search/', search_documents, name='document_search'),
     path('document/<int:document_id>/', document_detail, name='document_detail'),
 
